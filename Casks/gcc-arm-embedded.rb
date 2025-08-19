06806146d4c5a051da1f062eed3fcc8dd2f9b7dc
Pegerto Fernandez <pegerto@gmail.com>
2025-08-19 22:42:44 +0100

    arm support

--- Casks/gcc-arm-embedded.rb
+++ Casks/gcc-arm-embedded.rb
@@ -1,67 +1,74 @@
-cask "gcc-arm-embedded" do
+ask "gcc-arm-embedded" do
   # Exists as a cask because it is impractical as a formula:
   # https://github.com/Homebrew/homebrew-core/pull/45780#issuecomment-569246452
-  version "11.2-2022.02"
-  gcc_version = "11.2.1"
-  sha256 "105ac7f9b2be62c55d6853a099a92488c16f08bb296a694ef8430e5ddf8dead8"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://developer.arm.com/-/media/Files/downloads/gnu/#{version}/binrel/gcc-arm-#{version}-darwin-x86_64-arm-none-eabi.pkg"
-  name "GCC ARM Embedded"
-  desc "Pre-built GNU bare-metal toolchain for 32-bit Arm processors"
-  homepage "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain"
-
-  livecheck do
-    url "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/downloads"
-    regex(/href=.*?gcc-arm-(\d+\.\d+-\d+\.\d+)-darwin-(?:\w+)-arm-none-eabi.pkg/i)
+  pkg_version = nil
+  gcc_version = nil
+  on_arm do
+    version "11.3.Rel1"
+    sha256 "b93712026cec9f98a5d98dfec84e8096d32be3759642381e1982c4a5d2aa020b"
+    pkg_version = "11.3.Rel1"
+    gcc_version = "11.3"
+    livecheck do
+      url "https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads"
+      regex(/href=.*?arm-gnu-toolchain-(\d+\.\d+\.\w+)-darwin-(?:\w+)-arm-none-eabi\.pkg/i)
+    end
+  end
+  on_intel do
+    version "11.3"
+    sha256 "5d2e9ee4e73350bda79accc69fcd5ee59ccb902804a3f81a01d4c543b1ad7de7"
+    pkg_version = "11.3.Rel1"
+    gcc_version = "11.3"
+    livecheck do
+      skip "Legacy version"
+    end
   end
 
-  depends_on arch: :x86_64
+  url "https://developer.arm.com/-/media/Files/downloads/gnu/#{version}/binrel/arm-gnu-toolchain-#{version}-darwin-#{arch}-arm-none-eabi.pkg"
+  name "GCC ARM Embedded"
+  desc "Pre-built GNU bare-metal toolchain for 32-bit Arm processors"
+  homepage "https://developer.arm.com/Tools%20and%20Software/GNU%20Toolchain"
 
-  pkg "gcc-arm-#{version}-darwin-x86_64-arm-none-eabi.pkg"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-addr2line"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-ar"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-as"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-c++"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-c++filt"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-cpp"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-elfedit"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-g++"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-#{gcc_version}"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-ar"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-nm"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcc-ranlib"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcov"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcov-dump"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gcov-tool"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gdb"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gdb-add-index"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gfortran"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-gprof"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-ld"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-ld.bfd"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-lto-dump"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-nm"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-objcopy"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-objdump"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-ranlib"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-readelf"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-size"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-strings"
-  binary "#{appdir}/ARM/bin/arm-none-eabi-strip"
+  pkg "arm-gnu-toolchain-#{version}-darwin-#{arch}-arm-none-eabi.pkg"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-addr2line"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-ar"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-as"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-c++"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-c++filt"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-cpp"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-elfedit"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-g++"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcc"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcc-#{gcc_version}"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcc-ar"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcc-nm"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcc-ranlib"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcov"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcov-dump"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gcov-tool"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gdb"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gdb-add-index"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gfortran"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-gprof"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-ld"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-ld.bfd"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-lto-dump"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-nm"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-objcopy"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-objdump"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-ranlib"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-readelf"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-size"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-strings"
+  binary "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi/bin/arm-none-eabi-strip"
 
-  uninstall pkgutil: "gcc-arm-#{version}-darwin-x86_64-arm-none-eabi",
-            delete:  [
-              "/Applications/ARM/#{version}-darwin-x86_64-arm-none-eabi-manifest",
-              "/Applications/ARM/bin/arm-none-eabi-*",
-              "/Applications/ARM/arm-none-eabi",
-              "/Applications/ARM/lib/gcc/arm-none-eabi",
-              "/Applications/ARM/libexec/gcc/arm-none-eabi",
-              "/Applications/ARM/share/doc/arm-none-eabi",
-              "/Applications/ARM/share/gcc-arm-none-eabi",
-              "/Applications/ARM/share/man/man1/arm-none-eabi-*.1",
-              "/Applications/ARM/share/man/man5/arm-none-eabi-gdbinit.5",
+  uninstall pkgutil: "arm-gnu-toolchain-#{pkg_version}-darwin-#{arch}-arm-none-eabi",
+            delete:  "/Applications/ArmGNUToolchain/#{pkg_version}/arm-none-eabi",
+            rmdir:   [
+              "/Applications/ArmGNUToolchain",
+              "/Applications/ArmGNUToolchain/#{pkg_version}",
             ]
 
-  zap delete: "/Application/ARM"
+  # No zap stanza required
 end
